ccd26917e308e9c7abaad653a085ea19863ec5ff
LuisAP2831 <alquiciraluis2831@gmail.com>
2023-04-19 12:39:18 -0400

    Terminal casi hecha, checar por que se cierra

--- terminal.sh
+++ terminal.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+source ./creditos.sh
+source ./darHyF.sh
+source ./infosis.sh
+source ./juego.sh
+source ./mp3.sh
+source ./buscar.sh
+
 echo "Bienvenido a tu terminal"
 read -p "Por favor ingresa tu usuario: " nombre
 if id $nombre &> /dev/null; then
+   echo ingresa contraseña
+   su -c "whoami"
    echo "Bienvenido: $nombre"
 else
    echo "El usuario $nombre no existe"
@@ -14,18 +23,29 @@ fi
 
 
 while true; do
-printf "[%s@%s] " "$(whoami)" "$(basename" $(pwd)")"
+printf "[%s@%s] " "$(whoami)" "$(basename "$(pwd)")"
 read -a tarea
 case $tarea in
-     1)
-     echo uno
+     creditos)
+      creditos
+      true
      ;;
-     2)
-     echo dos
+     fecha)
+     darHyF
      ;;
-     3)
-     echo tres
+     infosis)
+     infosis
      ;;
+     ahorcado)
+     juego
+     ;;
+     mp3)
+     mp3
+     ;;
+     buscar)
+     buscar
+     ;;
+
      *)
      echo Comando no valido
 esac
