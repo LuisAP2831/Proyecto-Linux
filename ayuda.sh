a16d1bb115b39c363e0146e213123f019fa9286e
LuisAP2831 <alquiciraluis2831@gmail.com>
2023-04-21 00:14:03 -0600

    Ultimo comando wuu

---
+++ ayuda.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+function ayuda(){
+echo Haz ingresado al comando de ayuda, a continuacion te muestro los comandos disponibles
+echo   
+echo infosis : Permite saber las caracteristicas del equipo que se esta utilizando
+echo
+echo ahorcado : Usalo para activar el juego del ahorcado
+echo
+echo "mp3 : Reproductor de musica en mp3, descarga las canciones que quieras" 
+echo "(no olvides guardarlas en tu carpeta de Musica)"
+echo
+echo fecha : Te muestra la hora y fecha actual
+echo 
+echo buscar : Te permite buscar un archivo en especifico dentro de tu computadora.
+echo
+echo creditos : Utilizalo para conocer a los creadores de esta obra maestra
+}
